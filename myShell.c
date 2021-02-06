2cd1e9d97320e1c460fa581d56224a87febbb4dc
rnagoorm <rnagoorm@uoguelph.ca>
2021-02-07 01:08:34 +0400

    added asynchronous running of programs

--- myShell.c
+++ myShell.c
@@ -5,6 +5,7 @@
 #include <sys/wait.h>    /* Wait for Process Termination */
 #include <errno.h>       /* Errors */
 #include <string.h>      /* String Library */
+#include <signal.h>      /* Signal Library */
 
 char **strtoarray(char *line) {
     int i;
@@ -19,19 +20,31 @@ char **strtoarray(char *line) {
     return array;
 }
 
+void sigquit(int signo) {
+    printf(">");
+    exit(0);
+}
+
 int main(int argc, char **argv) {
     int exitcmd = 0;
     char cmdLine[1000], cmdLineTokens[1000];
     char *cmdLnArg;
-    char *cmdLnArg2;
     pid_t pid;
+    pid_t asynchpid;
     char **array;
     int status;
-    int i;
-    char* test[2]={"ls", "-l"};
+    
+    struct sigaction sigact;
+    sigact.sa_flags = 0;
+    sigemptyset(&sigact.sa_mask);
 
-    while (exitcmd == 0) {
-        printf(">");
+    sigact.sa_handler = sigquit;
+    if (sigaction(SIGQUIT, &sigact, NULL) < 0) {
+        perror("sigaction()");
+        exit(1);
+    }
+
+    while (exitcmd == 0) {//loop until exit is enterred
         fgets(cmdLine, 1000, stdin);
 
         //checking if there are too many character in the command line
@@ -44,30 +57,55 @@ int main(int argc, char **argv) {
             cmdLine[strlen(cmdLine)-1] = '\0';//remove the \n character
             strcpy(cmdLineTokens, cmdLine);
 
+            //to run command options
             cmdLnArg = strtok(cmdLineTokens, " ");//get the first arg from the argline
-            if (strcmp(cmdLnArg, "exit")==0) { //user enters the exit command
+
+            if (strcmp(cmdLnArg, "exit")==0) {//exit command
                 exitcmd = 1;
                 printf("myShell terminating...\n\n");
+                kill(asynchpid,SIGQUIT);
                 
-            } else if (strcmp(cmdLnArg, "ls")==0) {//user enters ls command
+            } else if (strcmp(cmdLnArg, "ls")==0) {//ls command
 
                 pid = fork();
-                if(pid >= 0) {
-                    if (pid == 0) {//child process
+                if(pid >= 0) {//if valid
+                    if (pid == 0) {//child runs process
                         array = strtoarray(cmdLine);
                         status = execvp("ls", array);
                         free(array);
                         exit(status);
                     } else if(pid > 0) {//parent process
                         waitpid(pid, &status, 0);
-                        if (WEXITSTATUS(status) == -1){
+                        if (WEXITSTATUS(status) == -1){//if it did not run successfully print error msg
                             perror("ls");
                         }
                     }
                 } else {
-                    perror("fork");
+                    perror("fork");//if fork failed
                 }
-            } else {
+
+            } else if (cmdLine[strlen(cmdLine)-1] == '&') {//asynchronous command
+                if ((asynchpid = fork()) < 0) {//if fork error
+                    perror("fork");
+                } else {//if fork was successfull
+                    if (asynchpid == 0) {//child runs process
+                        cmdLine[strlen(cmdLine)-1] = '\0';//remove & to get the valid array
+                        array = strtoarray(cmdLine);//get array for execvp
+                        status = execvp(cmdLnArg, array);//execute command
+                        if (status == -1) {//if failed to execute command
+                            perror(cmdLnArg);
+                        }
+                        printf(">");
+                    
+                        free(array);
+                        exit(status);
+                    } else {//parent process
+                        sigact.sa_handler = SIG_DFL;
+                        sigaction(SIGQUIT, &sigact, NULL);
+                    }
+                }
+
+            } else {//none of the previous options
                 printf("myShell: %s: command not found\n", cmdLine);
             }
             
