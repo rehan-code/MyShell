4af0b7b38b222161031ab24c993ae251eae489dc
rnagoorm <rnagoorm@uoguelph.ca>
2021-02-09 01:51:46 +0400

    added env variables, export, history and cd

--- myShell.c
+++ myShell.c
@@ -142,9 +142,66 @@ void pipeProcess(char **cmdline, int i) {
     }
 }
 
+void runProfileCmds(FILE *profilefileptr,char *path, char *histfile, char *home) {
+    char cmdline[1000], temp[1000];
+    char *cmdLnArg, *cmdLnArg2, *substring;
+    int i;
+
+    while (fgets(cmdline, 1000, profilefileptr)!=NULL) {
+        cmdline[strlen(cmdline)] = '\0';
+        if (cmdline[strlen(cmdline)-1] == '\n') {
+            cmdline[strlen(cmdline)-1] = '\0';
+        }
+        
+        cmdLnArg = strtok(cmdline, " \t");
+        cmdLnArg2 = strtok(NULL, " \t");
+
+        if (strcmp(cmdLnArg, "export")==0) {
+            //replace the environment variables with the values
+            strcpy(temp,cmdLnArg2);
+            if ((substring = strstr(cmdLnArg2, "$PATH")) != NULL) {
+                strcpy(temp, cmdLnArg2);
+                for (i = (strlen(cmdLnArg2)-strlen(substring)); i < 1000; i++) {
+                    temp[i]='\0';
+                }
+                strcat(temp, path);
+                strcat(temp, substring+5);
+                strcpy(cmdLnArg2, temp);
+            } else if ((substring = strstr(cmdLnArg2, "$HOME")) != NULL) {
+                strcpy(temp, cmdLnArg2);
+                for (i = (strlen(cmdLnArg2)-strlen(substring)); i < 1000; i++) {
+                    temp[i]='\0';
+                }
+                strcat(temp, home);
+                strcat(temp, substring+5);
+                strcpy(cmdLnArg2, temp);
+            } else if ((substring = strstr(cmdLnArg2, "$HISTFILE")) != NULL) {
+                strcpy(temp, cmdLnArg2);
+                for (i = (strlen(cmdLnArg2)-strlen(substring)); i < 1000; i++) {
+                    temp[i]='\0';
+                }
+                strcat(temp, histfile);
+                strcat(temp, substring+9);
+                strcpy(cmdLnArg2, temp);
+            }
+
+            //print the env variable values
+            if (strncmp(cmdLnArg2, "PATH=", 5)==0) {
+                strcpy(path, cmdLnArg2+5);
+            } else if (strncmp(cmdLnArg2, "HOME=", 5)==0) {
+                strcpy(home, cmdLnArg2+5);
+            } else if (strncmp(cmdLnArg2, "HISTFILE=", 9)==0) {
+                strcpy(histfile, cmdLnArg2+9);
+            }
+        }
+    }
+}
+
+
 int main(int argc, char **argv) {
     int exitcmd = 0;
-    char cmdLine[1000], cmdLineTokens[1000], histline[1000];
+    char cmdLine[1000], cmdLineTokens[1000], cmdLineTokens2[1000], histline[1000], cwd[1000], temp[1000];
+    char *substring;
     char *cmdLnArg;
     char *cmdLnArg2;
     pid_t pid;
@@ -153,29 +210,26 @@ int main(int argc, char **argv) {
     int status, i;
     int stploop = 0;
     int histLnNo = 0;
+    char c;
 
-    //environment variables
-    char *path = malloc(5);
+    //set and intialize environment variables
+    char *path = malloc(1000);
     strcpy(path, "/bin");
-    char *home = NULL;
-    home = getcwd(home,0);
-    char *histfile = malloc(strlen(home)+18);
+    char *home = malloc(1000);
+    home = getcwd(home,1000);
+    getcwd(cwd, 1000);
+    char *histfile = malloc(1000);
     strcpy(histfile, home);
     strcat(histfile, "/.CIS3110_history");
     FILE *histfileptr = fopen(histfile, "a+");
     if (histfileptr == NULL) {
         perror("histfileptr");
     }
-    FILE *profilefileptr = fopen(".CIS3110_profile", "a+");
-    if (profilefileptr == NULL) {
-        perror("profilefileptr");
-    }
-    
+    //get the line no. of the histfile (used when adding new entrees)
     fseek(histfileptr, 0, SEEK_END);
     if (ftell(histfileptr)==0) {
         histLnNo = 0;
     } else {
-        char c;
         fseek(histfileptr, 0, SEEK_SET);
         for (c = getc(histfileptr); c!=EOF; c = getc(histfileptr)) {
             if (c == '\n') {
@@ -183,7 +237,16 @@ int main(int argc, char **argv) {
             }
         }
     }
+    //initialize and run commands in profile file
+    FILE *profilefileptr = fopen(".CIS3110_profile", "a+");
+    if (profilefileptr == NULL) {
+        perror("profilefileptr");
+    } else {
+        runProfileCmds(profilefileptr, path, histfile, home);
+        fclose(profilefileptr);
+    }
 
+    //initialize sigaction
     struct sigaction sigact;
     sigact.sa_flags = 0;
     sigemptyset(&sigact.sa_mask);
@@ -196,43 +259,48 @@ int main(int argc, char **argv) {
 
     while (exitcmd == 0) {//loop until exit is entered
         stploop = 0;
-        printf(">");
+        printf("%s> ",cwd);
         fgets(cmdLine, 1000, stdin);
 
         //checking if there are too many character in the command line
         if (strlen(cmdLine) == 999 && cmdLine[998] != '\n') {
-            while (fgets(cmdLine, 1000, stdin) != NULL) {   
+            while (fgets(cmdLine, 1000, stdin) != NULL) {
             }
             printf("myShell: too many characters\n");
 
         } else {//if valid no. of arguments, run rest of the program
+            //add new command to the history file
             fseek(histfileptr, 0, SEEK_END);
             fprintf(histfileptr, " %d  %s", ++histLnNo, cmdLine);
             cmdLine[strlen(cmdLine)-1] = '\0';//remove the \n character
             strcpy(cmdLineTokens, cmdLine);
 
-            array = strtoarray(cmdLineTokens);
-            //change the input or output stream depending on the choice
+            //get the first and second arg from the argline
+            cmdLnArg = strtok(cmdLineTokens, " \t");
+            cmdLnArg2 = strtok(NULL, " \t");
+            strcpy(cmdLineTokens2, cmdLine);
+
+            
+
+
+            //To run redirection pipe commands
+            array = strtoarray(cmdLineTokens2);
             for (i = 0; array[i]!=NULL && stploop == 0; i++) {
                 if (strcmp(array[i], ">")==0) {//To change the output stream
                     redOutProcess(array, i);
                     stploop = 1;
-                } else if (strcmp(array[i], "<")==0) {
+                } else if (strcmp(array[i], "<")==0) {//change the input stream
                     redInpProcess(array, i);
                     stploop = 1;
-                } else if (strcmp(array[i], "|")==0) {
+                } else if (strcmp(array[i], "|")==0) {//pipe
                     pipeProcess(array, i);
                     stploop = 1;
                 }
             }
             free(array);
-            strcpy(cmdLineTokens, cmdLine);
 
-            if (stploop == 0) {
-                
-                //to run command options
-                cmdLnArg = strtok(cmdLineTokens, " \t");//get the first arg from the argline
-                cmdLnArg2 = strtok(NULL, " \t");
+
+            if (stploop == 0) {//loop to run all other commands
 
                 if (strcmp(cmdLnArg, "exit")==0) {//exit command
                     exitcmd = 1;
@@ -241,7 +309,6 @@ int main(int argc, char **argv) {
                     free(histfile);
                     free(home);
                     fclose(histfileptr);
-                    fclose(profilefileptr);
                     kill(asynchpid,SIGQUIT);
 
                 } else if (cmdLine[strlen(cmdLine)-1] == '&') {//asynchronous command
@@ -255,7 +322,7 @@ int main(int argc, char **argv) {
                             if (status == -1) {//if failed to execute command
                                 perror(cmdLnArg);
                             }
-                            printf(">");
+                            printf("> ");
                         
                             free(array);
                             exit(status);
@@ -265,6 +332,7 @@ int main(int argc, char **argv) {
                         }
                     }
 
+                //the 3 echo options for path, histfile and home
                 }else if (strcmp(cmdLnArg, "echo")==0 && strcmp(cmdLnArg2, "$PATH")==0) {
                     printf("%s\n", path);
 
@@ -274,12 +342,25 @@ int main(int argc, char **argv) {
                 }else if (strcmp(cmdLnArg, "echo")==0 && strcmp(cmdLnArg2, "$HOME")==0) {
                     printf("%s\n", home);
 
-                }else if (strcmp(cmdLnArg, "history")==0) {
-                    if (cmdLnArg2!=NULL && strcmp(cmdLnArg2, "-c")==0) {
-                        histfileptr = freopen(histfile, "w+", histfileptr);
+                }else if (strcmp(cmdLnArg, "history")==0) {//history command
+                    if (cmdLnArg2!=NULL && strcmp(cmdLnArg2, "-c")==0) {//-c clears history file
+                        histfileptr = freopen(histfile, "w+", histfileptr);//reopen the file causes the file to be erased
+                        histLnNo = 0;
 
-                    } else if (cmdLnArg2!=NULL && atoi(cmdLnArg2)!=0) {
-                        fseek(histfileptr, -(atoi(cmdLnArg2)), SEEK_END);
+                    } else if (cmdLnArg2!=NULL && atoi(cmdLnArg2)!=0) {//n prints last n lines
+                        
+                        fseek(histfileptr, -1, SEEK_END);
+                        for (c = getc(histfileptr), i=0; i!=atoi(cmdLnArg2)+1 && i<=histLnNo; c = getc(histfileptr)) {
+                            if (c == '\n') {
+                                i++;
+                            }
+                            fseek(histfileptr, -2, SEEK_CUR);
+                        }
+                        fseek(histfileptr, 1, SEEK_CUR);
+                        if (i==histLnNo) {
+                            fseek(histfileptr, 0, SEEK_SET);
+                        }
+                        
                         while (fgets(histline, 1000, histfileptr)!=NULL) {
                             printf("%s", histline);
                         }
@@ -291,8 +372,52 @@ int main(int argc, char **argv) {
                         }
                     }
 
-                } else {//execute a command
+                }else if (strcmp(cmdLnArg, "export")==0) {
+                    //replace the environment variables with the values
+                    strcpy(temp,cmdLnArg2);
+                    if ((substring = strstr(cmdLnArg2, "$PATH")) != NULL) {
+                        strcpy(temp, cmdLnArg2);
+                        for (i = (strlen(cmdLnArg2)-strlen(substring)); i < 1000; i++) {
+                            temp[i]='\0';
+                        }
+                        strcat(temp, path);
+                        strcat(temp, substring+5);
+                        strcpy(cmdLnArg2, temp);
+                    } else if ((substring = strstr(cmdLnArg2, "$HOME")) != NULL) {
+                        strcpy(temp, cmdLnArg2);
+                        for (i = (strlen(cmdLnArg2)-strlen(substring)); i < 1000; i++) {
+                            temp[i]='\0';
+                        }
+                        strcat(temp, home);
+                        strcat(temp, substring+5);
+                        strcpy(cmdLnArg2, temp);
+                    } else if ((substring = strstr(cmdLnArg2, "$HISTFILE")) != NULL) {
+                        strcpy(temp, cmdLnArg2);
+                        for (i = (strlen(cmdLnArg2)-strlen(substring)); i < 1000; i++) {
+                            temp[i]='\0';
+                        }
+                        strcat(temp, histfile);
+                        strcat(temp, substring+9);
+                        strcpy(cmdLnArg2, temp);
+                    }
 
+                    //print the env variable values
+                    if (strncmp(cmdLnArg2, "PATH=", 5)==0) {
+                        strcpy(path, cmdLnArg2+5);
+                    } else if (strncmp(cmdLnArg2, "HOME=", 5)==0) {
+                        strcpy(home, cmdLnArg2+5);
+                    } else if (strncmp(cmdLnArg2, "HISTFILE=", 9)==0) {
+                        strcpy(histfile, cmdLnArg2+9);
+                    }
+                    
+                }else if (strcmp(cmdLnArg, "cd")==0) {//cd function
+                    if(chdir(cmdLnArg2)==0){
+                        getcwd(cwd,1000);
+                    } else {
+                        perror("cd");
+                    }
+
+                } else {//execute a command
                     pid = fork();
                     if(pid >= 0) {//if valid
                         if (pid == 0) {//child runs process
@@ -300,7 +425,7 @@ int main(int argc, char **argv) {
                             status = execvp(array[0], array);
                             free(array);
                             exit(status);
-                        } else if(pid > 0) {//parent process
+                        } else {//parent process
                             waitpid(pid, &status, 0);
                             if (WEXITSTATUS(status) == -1){//if it did not run successfully print error msg
                                 perror(cmdLine);
